3616b0b78cc0fc38e7bfbf41ae2a551bc4ae46c6
Golbakh Mikhail <golbah@yandex.ru>
2020-04-15 00:22:27 +0300

    Сделал новый компонент Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import useStyles from 'isomorphic-style-loader/useStyles';
+
+import s from './Button.scss';
+import { cn } from '../../utils/bem-css-module';
+
+type ButtonShape = 'right' | 'left';
+
+type ButtonColor = 'blue' | 'green' | 'gray' | 'bordered';
+
+type ButtonSize = 'xs' | 's' | 'm';
+
+export interface ButtonProps {
+    shape?: ButtonShape;
+    color?: ButtonColor;
+    size?: ButtonSize;
+    children: React.ReactText;
+}
+
+/** TODO: remove this
+ * 44 x 18 ?
+ * 60 x 35
+ * 85 x 40 - with icon
+ * 125 x 33
+ * 140 x 55 ?
+ * 165 x 40
+ * 180 x 55 ?
+ * 190 x 45 ?
+ * 200 x 50 ?
+ */
+
+const cnButton = cn(s, 'Button');
+
+const Button: React.FC<ButtonProps> = ({ shape = 'right', color = 'blue', size = 's', children }) => {
+    useStyles(s);
+
+    return (
+        <button className={cnButton({ shape, color, size })} type="button">
+            {children}
+        </button>
+    );
+};
+
+export default Button;
