8221d5ee6e6ee2fb3dcd3084399532be09e83223
Golbakh Mikhail <golbah@yandex.ru>
2020-04-14 18:40:59 +0300

    Добавил компонент Icon

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import useStyles from 'isomorphic-style-loader/useStyles';
+
+import { cn } from '../../utils/bem-css-module';
+import s from './Icon.scss';
+
+type IconSize = 'xs' | 's' | 'm';
+
+type IconType =
+    | 'gamepad'
+    | 'ingots'
+    | 'play'
+    | 'user'
+    | 'lightning'
+    | 'rub'
+    | 'cardsCut'
+    | 'ingotsCut'
+    | 'playRedCut'
+    | 'guard'
+    | 'diamondWhite';
+
+interface IconProps {
+    size?: IconSize;
+    type: IconType;
+}
+
+const cnIcon = cn(s, 'Icon');
+
+const Icon: React.FC<IconProps> = ({ size = 's', type }) => {
+    useStyles(s);
+
+    return <i className={cnIcon({ type, size })} />;
+};
+
+export default Icon;
