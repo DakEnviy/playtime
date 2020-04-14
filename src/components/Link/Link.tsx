70af39d81d56e1b75c08f747550bc66d63d6b67e
Golbakh Mikhail <golbah@yandex.ru>
2020-04-14 18:40:22 +0300

    Улучшил Link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,37 +1,40 @@
-import React from 'react';
+import React, { MouseEvent as ReactMouseEvent } from 'react';
 
 import history from '../../history';
 import { isLeftClickEvent, isModifiedEvent } from '../../utils/link';
 
 interface LinkProps {
     to: string;
+    external?: boolean;
+    newTab?: boolean;
     onClick?: Function;
+
     className?: string;
 }
 
-const Link: React.FC<LinkProps> = ({ to, onClick, children, ...restProps }) => (
-    <a
-        href={to}
-        {...restProps}
-        onClick={event => {
-            if (onClick) {
-                onClick(event);
-            }
+const Link: React.FC<LinkProps> = ({ to, external, newTab, onClick, children, ...restProps }) => {
+    const onLinkClick = (event: ReactMouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        if (onClick) {
+            onClick(event);
+        }
 
-            if (isModifiedEvent(event) || !isLeftClickEvent(event)) {
-                return;
-            }
+        if (isModifiedEvent(event) || !isLeftClickEvent(event)) {
+            return;
+        }
 
-            if (event.defaultPrevented) {
-                return;
-            }
+        if (event.defaultPrevented) {
+            return;
+        }
 
-            event.preventDefault();
-            history.push(to);
-        }}
-    >
-        {children}
-    </a>
-);
+        event.preventDefault();
+        history.push(to);
+    };
+
+    return (
+        <a href={to} target={newTab ? '_blank' : undefined} onClick={external ? undefined : onLinkClick} {...restProps}>
+            {children}
+        </a>
+    );
+};
 
 export default Link;
