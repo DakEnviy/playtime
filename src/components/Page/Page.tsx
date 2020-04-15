9a482453033af02a781b0ea01227ef997f93e579
Golbakh Mikhail <golbah@yandex.ru>
2020-04-15 06:19:09 +0300

    Добавил overflow для Page

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -3,15 +3,18 @@ import useStyles from 'isomorphic-style-loader/useStyles';
 import normalizeCss from 'normalize.css';
 
 import s from './Page.scss';
+import { cn } from '../../utils/bem-css-module';
 
-interface PageProps {
+export interface PageProps {
     children: React.ReactNode;
 }
+
+const cnPage = cn(s, 'Page');
 
 const Page: React.FC<PageProps> = ({ children }) => {
     useStyles(normalizeCss, s);
 
-    return <>{children}</>;
+    return <div className={cnPage()}>{children}</div>;
 };
 
 export default Page;
