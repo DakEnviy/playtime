e771de89c5fc8250b5360b5f4c92c83938cbd031
Golbakh Mikhail <golbah@yandex.ru>
2020-04-18 00:42:04 +0300

    Убрал ошибку на оператор инкремента

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,6 +42,8 @@ module.exports = {
             },
         ],
 
+        'no-plusplus': 'off',
+
         // Recommend not to leave any console.log in your code
         // Use console.error, console.warn and console.info instead
         // https://eslint.org/docs/rules/no-console
