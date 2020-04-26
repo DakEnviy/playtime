34a7616f961597f9d36cbee8fbfcb31ac92a64cb
Golbakh Mikhail <golbah@yandex.ru>
2020-04-26 06:46:06 +0300

    Добавил возможность отменять обязательность написания резолвера в маппинге

--- src/interfaces/graphql.ts
+++ src/interfaces/graphql.ts
@@ -2,6 +2,16 @@ import { GraphQLResolveInfo, GraphQLScalarType } from 'graphql';
 
 export type MayPromise<T> = T | Promise<T>;
 
+export type MustBeSubType<SubType extends object, Type extends object> = {
+    [P in keyof SubType]: P extends keyof Type ? unknown : never;
+};
+
+export type FilterFieldsByType<TObject extends object, TType> = {
+    [P in keyof TObject]: TObject[P] extends TType ? P : never;
+};
+
+export type AllowedNames<TObject extends object, TType> = FilterFieldsByType<TObject, TType>[keyof TObject];
+
 export interface FieldResolver<TContext, TParent, TArgs, TResult> {
     (parent: TParent, args: TArgs, context: TContext, info: GraphQLResolveInfo): MayPromise<TResult>;
 }
@@ -10,10 +20,6 @@ export interface Resolver<TResult, TArgs = {}> {
     result: TResult;
     args: TArgs;
 }
-
-export type MustBeSubType<SubType extends object, Type extends object> = {
-    [P in keyof SubType]: P extends keyof Type ? unknown : never;
-};
 
 export interface TypeResolvers<
     TType extends object,
@@ -33,7 +39,7 @@ export interface EnumResolver<TType extends string | number | symbol, TValue> {
 export type Resolvers<TResolvers extends object, TContext> = {
     [P in keyof TResolvers]: TResolvers[P] extends TypeResolvers<infer TType, infer TMapping, infer TParent>
         ? {
-              [K in keyof Required<TType>]: K extends keyof TMapping
+              [K in Exclude<keyof Required<TType>, AllowedNames<TMapping, void>>]: K extends keyof TMapping
                   ? TMapping[K] extends Resolver<infer TResult, infer TArgs>
                       ? FieldResolver<TContext, TParent, TArgs, TResult>
                       : FieldResolver<TContext, TParent, {}, TMapping[K]>
