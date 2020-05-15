b529bb58f639acffef058ae1f64ac7ed65299aad
Golbakh Mikhail <golbah@yandex.ru>
2020-05-15 15:53:09 +0300

    Пофиксил баг с изоморфностью скаляра DateTime

--- src/data/modules/scalars/resolvers.ts
+++ src/data/modules/scalars/resolvers.ts
@@ -1,4 +1,4 @@
-import { GraphQLURL, GraphQLDateTime } from 'graphql-custom-types';
+import { GraphQLURL } from 'graphql-custom-types';
 import { GraphQLScalarType } from 'graphql';
 
 import { Resolvers } from '../../../interfaces/graphql';
@@ -13,7 +13,12 @@ type ScalarResolvers = Resolvers<
 
 const resolvers: ScalarResolvers = {
     URL: GraphQLURL,
-    DateTime: GraphQLDateTime,
+    DateTime: new GraphQLScalarType({
+        name: 'DateTime',
+        description: 'A date and time, represented as an ISO-8601 string',
+        serialize: value => value.toISOString(),
+        parseValue: value => new Date(value),
+    }),
 };
 
 export default resolvers;
